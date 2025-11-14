db8afd56aa65697f9df47867da4b7f5e5a05aebd
hema-subburaman <iamhema2k6@gmail.com>
2025-11-14 18:58:13 +0530

    Movie Space

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// 5235b120
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import search from "./search.svg"
+import MovieCard from './MovieCard'
+
+
+const API_URL = 'http://www.omdbapi.com/?apikey=5235b120';
+
+function App() {
+
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL} &s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovie("Batman")
+  }, [])
+  
+
+  return (
+    <>
+      <div className="app">
+        <h1>MovieSpace</h1>
+
+        <div className="search">
+          <input placeholder='Search for movies'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)} />
+
+          <img src={search} alt="search" onClick={() => searchMovie(searchTerm)} />
+        </div>
+
+        {movies?.length > 0 ?(
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie = {movie}/>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies Found</h2>
+          </div>
+        )}
+
+      
+        
+      </div>
+    </>
+  )
+}
+
+export default App
